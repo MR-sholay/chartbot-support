13e8ceb42261f0ce789e1c13c082a6e5ab9b8b66
MR-sholay <accpersonal.study@gmail.com>
2025-04-01 23:29:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,5 +73,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int("5000") 
     app.run(host="0.0.0.0", port=port)
